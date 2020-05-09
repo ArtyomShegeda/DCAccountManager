be17ff0f4769789c440250bae55723464f781ef4
Artyom Shegeda <shegeda@ukr.net>
2020-05-09 17:03:45 +0300

    Initial commit

---
+++ DragonCityAccountsManager/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace DragonCityAccountsManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 94);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(516, 356);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 180;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Account ID";
+            this.columnHeader2.Width = 90;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 94);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(137, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(188, 29);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Account ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(414, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Activate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DC Accounts Manager";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DragonCityAccountsManager/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DragonCityAccountsManager
+{
+    public partial class Form1 : Form
+    {
+        private static string SettingsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DragonCityAccountsManager");
+        private static string SettingsFilePath = Path.Combine(SettingsFolder, "accounts.xml");
+        Dictionary<string, dynamic> accounts = new Dictionary<string, dynamic>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadAccounts();
+            DetectNewAccount();
+            RenderAccounts();
+        }
+
+        private void DetectNewAccount()
+        {
+            var roamingPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Social Point", "DragonCity");
+            var localPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Social Point", "DragonCity");
+
+            if (File.Exists(Path.Combine(roamingPath, "folderAttrStorage-userId.json")) 
+                && File.Exists(Path.Combine(roamingPath, "folderAttrStorage-securityToken.json"))
+                && File.Exists(Path.Combine(localPath, "folderAttrStorage-userId.json"))
+                && File.Exists(Path.Combine(localPath, "folderAttrStorage-securityToken.json")))
+            {
+                var id = File.ReadAllText(Path.Combine(localPath, "folderAttrStorage-userId.json"));
+                var securityToken = File.ReadAllText(Path.Combine(localPath, "folderAttrStorage-securityToken.json"));
+
+                if (!accounts.ContainsKey(id))
+                    accounts.Add(id, CreateAccount(id, id, securityToken));
+
+                SaveAccounts();
+            }
+            else
+            {
+                MessageBox.Show("Sorry.\nCannot detect Dragon City game settings.");
+            }
+        }
+
+        private void RenderAccounts()
+        {
+            foreach (var account in accounts.Values)
+            {
+                if (listView1.Items[account.id] == null)
+                {
+                    listView1.Items.Add(new ListViewItem(new string[] { account.name, account.id })
+                    {
+                        Name = account.id
+                    });
+                }
+            }
+        }
+
+        private void LoadAccounts()
+        {
+            if (!File.Exists(SettingsFilePath))
+                return;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(SettingsFilePath);
+
+            foreach(XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                var account = CreateAccount(
+                    node.Attributes["id"].Value, 
+                    node.Attributes["name"].Value,
+                    node.Attributes["securityToken"].Value);
+                accounts.Add(account.id, account);
+            }
+        }
+
+        private dynamic CreateAccount(string id, string name, string securityToken)
+        {
+            return new
+            {
+                name,
+                id,
+                securityToken
+            };
+        }
+
+        private void SaveAccounts()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.AppendChild(doc.CreateElement("accounts"));
+
+            foreach (var account in accounts.Values)
+            {
+                var node = doc.CreateElement("account");
+                var attribute = doc.CreateAttribute("name");
+                attribute.Value = account.name;
+                node.Attributes.Append(attribute);
+                attribute = doc.CreateAttribute("id");
+                attribute.Value = account.id;
+                node.Attributes.Append(attribute);
+                attribute = doc.CreateAttribute("securityToken");
+                attribute.Value = account.securityToken;
+                node.Attributes.Append(attribute);
+                doc.DocumentElement.AppendChild(node);
+            }
+
+            Directory.CreateDirectory(SettingsFolder);
+            doc.Save(SettingsFilePath);
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 1)
+                return;
+            var id = listView1.SelectedItems[0].Name;
+            if (accounts[id].name != textBox1.Text) {
+                accounts[id] = CreateAccount(id, textBox1.Text, accounts[id].securityToken);
+                listView1.SelectedItems[0].Text = textBox1.Text;
+                SaveAccounts();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+                button1.Enabled = false;
+                return;
+            }
+            if (listView1.SelectedItems.Count != 1)
+                return;
+
+            var id = listView1.SelectedItems[0].Name;
+            textBox1.Text = accounts[id].name;
+            textBox2.Text = id;
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var roamingPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Social Point", "DragonCity");
+            var localPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Social Point", "DragonCity");
+
+            var id = listView1.SelectedItems[0].Name;
+            var account = accounts[id];
+            File.WriteAllText(Path.Combine(localPath, "folderAttrStorage-userId.json"), id);
+            File.WriteAllText(Path.Combine(localPath, "folderAttrStorage-securityToken.json"), account.securityToken);
+            File.WriteAllText(Path.Combine(roamingPath, "folderAttrStorage-userId.json"), id);
+            File.WriteAllText(Path.Combine(roamingPath, "folderAttrStorage-securityToken.json"), account.securityToken);
+        }
+    }
+}
